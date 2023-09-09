7985b1b1161385b1d85d635460f190e7169747b1
Michael K <1747304+tsal@users.noreply.github.com>
2023-09-09 13:53:01 -0400

    fix: remove example message handler

--- src/main.rs
+++ src/main.rs
@@ -22,14 +22,6 @@ struct Bot {
 
 #[async_trait]
 impl EventHandler for Bot {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if msg.content == "!hello" {
-            if let Err(e) = msg.channel_id.say(&ctx.http, "world!").await {
-                error!("Error sending message: {:?}", e);
-            }
-        }
-    }
-
     async fn ready(&self, ctx: Context, ready: Ready) {
         info!("{} is connected!", ready.user.name);
         let guild_id = GuildId(self.owner_guild_id);
